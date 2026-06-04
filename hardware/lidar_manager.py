d4f0a32230c2f5e63b01bc492dde497d425f4d39
DongjuLee <dozoo0528@gmail.com>
2026-06-04 20:11:41 +0900

    refactor: remove hardcoding, add exception handling, sync config.py

--- hardware/lidar_manager.py
+++ hardware/lidar_manager.py
@@ -6,22 +6,22 @@ from utils.config import *
 class LidarManager:
     def __init__(self):
         self.serial_port = serial.Serial(LIDAR_UART_PORT, LIDAR_BAUDRATE, timeout=1)
-        self.obstacle_threshold = 500
-        self.directions = [0, 45, 90, 135, 180, 225, 270, 315]
+        self.obstacle_threshold = LIDAR_OBSTACLE_THRESHOLD
+        self.directions = LIDAR_DIRECTION_ANGLES
 
     def read_scan(self):
         try:
             distance_data = {}
             while True:
-                data = self.serial_port.read(42)
-                if len(data) == 42 and data[0] == 0xAA and data[1] == 0xAA:
+                data = self.serial_port.read(LIDAR_DATA_SIZE)
+                if len(data) == LIDAR_DATA_SIZE and data[0] == LIDAR_PACKET_HEADER[0] and data[1] == LIDAR_PACKET_HEADER[1]:
                     angle = (data[4] | (data[5] << 8)) / 100.0
                     for i in range(12):
                         point_distance = data[6 + 3*i] | (data[7 + 3*i] << 8)
                         point_angle = angle + i * 0.5
                         if point_distance > 0:
                             distance_data[point_angle] = point_distance
-                    if len(distance_data) >= 360:
+                    if len(distance_data) >= LIDAR_FULL_SCAN_SIZE:
                         break
             return distance_data
         except Exception as e:
@@ -32,10 +32,10 @@ class LidarManager:
         if not scan_data:
             return [False] * 8
 
-        obstacles = [False] * 8
+        obstacles = [False] * LIDAR_DIRECTION_COUNT
         for i, direction in enumerate(self.directions):
             min_distance = float('inf')
-            for angle in range(direction - 22, direction + 23):
+            for angle in range(direction - LIDAR_ANGLE_RANGE, direction + LIDAR_ANGLE_RANGE + 1):
                 normalized_angle = angle % 360
                 if normalized_angle in scan_data:
                     min_distance = min(min_distance, scan_data[normalized_angle])
@@ -54,7 +54,7 @@ class LidarManager:
                 clear_directions.append(self.directions[i])
 
         if not clear_directions:
-            return 180
+            return LIDAR_REVERSE_DIRECTION
 
         if 0 in clear_directions:
             return 0
@@ -75,7 +75,7 @@ class LidarManager:
             return False
 
         min_distance = float('inf')
-        for angle in range(direction_deg - 10, direction_deg + 11):
+        for angle in range(direction_deg - LIDAR_PATH_CHECK_RANGE, direction_deg + LIDAR_PATH_CHECK_RANGE + 1):
             normalized_angle = angle % 360
             if normalized_angle in scan_data:
                 min_distance = min(min_distance, scan_data[normalized_angle])
