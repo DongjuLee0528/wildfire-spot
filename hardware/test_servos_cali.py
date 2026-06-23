0356083b913b974d283e2e65bf17c810d663ba59
DongjuLee <dozoo0528@gmail.com>
2026-06-23 22:15:38 +0900

    fix(hardware): improve runtime safety and hardware compatibility

--- hardware/test_servos_cali.py
+++ hardware/test_servos_cali.py
@@ -1,23 +1,9 @@
-from adafruit_servokit import ServoKit
-import board
-import busio
 import time
 
 # On the Jetson Nano
 # Bus 0 (pins 28,27) is board SCL_1, SDA_1 in the jetson board definition file
 # Bus 1 (pins 5, 3) is board SCL, SDA in the jetson definition file
 # Default is to Bus 1; We are using Bus 0, so we need to construct the busio first ...
-print("Initializing Servos")
-i2c_bus0=(busio.I2C(board.SCL_1, board.SDA_1))
-print("Initializing ServoKit")
-
-kit = ServoKit(channels=16, i2c=i2c_bus0, address=0x40)
-kit2 = ServoKit(channels=16, i2c=i2c_bus0, address=0x41)
-
-# kit[0] is the front servos
-# kit[1] is the rear servos
-print("Done initializing")
-
 # [0]~[2] : FL // [3]~[5] : FR // [6]~[8] : RL // [9]~[11] : RR
 val_list = [90]
 prev_angle = None
@@ -25,11 +11,30 @@ cur_angle = None
 # val_list = [90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90]
 
 
-if __name__ == '__main__':
+def main():
+    from adafruit_servokit import ServoKit
+    import board
+    import busio
+
+    print("Initializing Servos")
+    i2c_bus0=(busio.I2C(board.SCL_1, board.SDA_1))
+    print("Initializing ServoKit")
+
+    kit = ServoKit(channels=16, i2c=i2c_bus0, address=0x40)
+    kit2 = ServoKit(channels=16, i2c=i2c_bus0, address=0x41)
+
+    # kit[0] is the front servos
+    # kit[1] is the rear servos
+    print("Done initializing")
+
+    global prev_angle, cur_angle
 
     while True:
         # motor_num is index of motor to rotate
-        motor_num=int(input("Enter Servo to rotate (0-11): "))
+        motor_num=int(input(f"Enter Servo to rotate (0-{len(val_list) - 1}): "))
+        if motor_num < 0 or motor_num >= len(val_list):
+            print(f"Invalid servo number: {motor_num}")
+            continue
         
         # new angle to be written on selected motor
         cur_angle=int(input("Enter new angles (0-180): "))
@@ -52,3 +57,7 @@ if __name__ == '__main__':
 
 
         prev_angle = val_list[motor_num]
+
+
+if __name__ == '__main__':
+    main()
