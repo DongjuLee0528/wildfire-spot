83f6900f794522dc5a81033d752338936dc783fa
DongjuLee <dozoo0528@gmail.com>
2026-07-03 13:30:16 +0900

    docs(server): add documentation comments for server modules

--- server/src/main/java/com/wildfirespot/server/adapter/RobotGpsProvider.java
+++ server/src/main/java/com/wildfirespot/server/adapter/RobotGpsProvider.java
@@ -2,6 +2,7 @@ package com.wildfirespot.server.adapter;
 
 import com.wildfirespot.server.dto.GpsResponse;
 
+/** Provides the latest GPS coordinates and fix status. */
 public interface RobotGpsProvider {
     GpsResponse getGps();
 }
